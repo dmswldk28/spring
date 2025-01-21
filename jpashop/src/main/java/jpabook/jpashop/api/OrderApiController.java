630629f11c3c3e76ec9be7b2ecb4a74b63f0e3f5
이은지 <dmswldk28@gmail.com>
2025-01-21 17:55:55 +0900

    jpa2

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
@@ -38,6 +40,7 @@ import lombok.RequiredArgsConstructor;
  */
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "주문관리 API", description = "컬렉션 조회 최적화")
 public class OrderApiController {
 	
 	private final OrderRepository orderRepository;
@@ -90,7 +93,6 @@ public class OrderApiController {
 			orderItems = order.getOrderItems().stream()
 					.map(orderItem -> new OrderItemDto(orderItem))
 					.collect(Collectors.toList());
-		
 		}
 	}
 	
@@ -108,4 +110,27 @@ public class OrderApiController {
 		}
 	}
 
+	@GetMapping("/api/v3/orders")
+	public List<OrderDto> ordersV3() {
+		List<Order> orders = orderRepository.findAllWithItem();
+		List<OrderDto> result = orders.stream()
+				.map(o -> new OrderDto(o))
+				.collect(Collectors.toList());
+		return result;
+	}
+	
+	/**
+	 * V3.1 엔티티를 조회해서 DTO로 변환 페이징 고려
+	 * - ToOne 관계만 우선 모두 페치 조인으로 최적화
+	 * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+	 */
+	@GetMapping("/api/v3.1/orders")
+	public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+										@RequestParam(value = "limit", defaultValue = "100") int limit) {
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+		List<OrderDto> result = orders.stream()
+				.map(o -> new OrderDto(o))
+				.collect(Collectors.toList());
+		return result;
+	}
 }
